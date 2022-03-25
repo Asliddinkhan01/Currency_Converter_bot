23bce587f26cac700ef2eaa9aee96df3365a5d0c
Asliddinkhan01 <asliddinkenjaev17@gmail.com>
2022-03-25 09:04:13 +0500

    Constants changed

--- src/main/java/uz/asliddin/bot/Constants.java
+++ src/main/java/uz/asliddin/bot/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final String botToken = ""; //add your bot token
     public static final String ACCOUNT_SID = ""; //add your twilio sid
     public static final String AUTH_TOKEN = ""; //add your twilio token
-    public static final String MESSAGE_SID = " "; //add your twilio sid
+    public static final String MESSAGE_SID = ""; //add your twilio sid
 }
